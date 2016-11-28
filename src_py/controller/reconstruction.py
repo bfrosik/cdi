5f52fb81dcbcc014d540275e9be9302fe0f97a7b
Barbara Frosik <bfrosik@anl.gov>
2016-11-28 15:07:04 -0600

    comments

--- src_py/controller/reconstruction.py
+++ src_py/controller/reconstruction.py
@@ -1,10 +1,89 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# #########################################################################
+# Copyright (c) 2016, UChicago Argonne, LLC. All rights reserved.         #
+#                                                                         #
+# Copyright 2016. UChicago Argonne, LLC. This software was produced       #
+# under U.S. Government contract DE-AC02-06CH11357 for Argonne National   #
+# Laboratory (ANL), which is operated by UChicago Argonne, LLC for the    #
+# U.S. Department of Energy. The U.S. Government has rights to use,       #
+# reproduce, and distribute this software.  NEITHER THE GOVERNMENT NOR    #
+# UChicago Argonne, LLC MAKES ANY WARRANTY, EXPRESS OR IMPLIED, OR        #
+# ASSUMES ANY LIABILITY FOR THE USE OF THIS SOFTWARE.  If software is     #
+# modified to produce derivative works, such modified software should     #
+# be clearly marked, so as not to confuse it with the version available   #
+# from ANL.                                                               #
+#                                                                         #
+# Additionally, redistribution and use in source and binary forms, with   #
+# or without modification, are permitted provided that the following      #
+# conditions are met:                                                     #
+#                                                                         #
+#     * Redistributions of source code must retain the above copyright    #
+#       notice, this list of conditions and the following disclaimer.     #
+#                                                                         #
+#     * Redistributions in binary form must reproduce the above copyright #
+#       notice, this list of conditions and the following disclaimer in   #
+#       the documentation and/or other materials provided with the        #
+#       distribution.                                                     #
+#                                                                         #
+#     * Neither the name of UChicago Argonne, LLC, Argonne National       #
+#       Laboratory, ANL, the U.S. Government, nor the names of its        #
+#       contributors may be used to endorse or promote products derived   #
+#       from this software without specific prior written permission.     #
+#                                                                         #
+# THIS SOFTWARE IS PROVIDED BY UChicago Argonne, LLC AND CONTRIBUTORS     #
+# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT       #
+# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS       #
+# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL UChicago     #
+# Argonne, LLC OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,        #
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,    #
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;        #
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER        #
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT      #
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN       #
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE         #
+# POSSIBILITY OF SUCH DAMAGE.                                             #
+# #########################################################################
+
+"""
+Please make sure the installation :ref:`pre-requisite-reference-label` are met.
+This module controls the reconstruction process. The user has to provide parameters such as type of processor, data, and configuration.
+The processor specifies which library will be used by CFM (Calc Fast Module) that performs the processor intensive calculations. The module
+can be run on cpu, or gpu. Depending on the gpu hardware and library, one can use opencl or cuda library.
+The module starts the data preparation routines, calls for reconstruction using the CFM, and prepares the reconstructed data for
+visualization.
+"""
+
 import numpy as np
 import src_py.utilities.utils as ut
 import pylibconfig2 as cfg
 import os
 import scipy.fftpack as sf
 
+__author__ = "Barbara Frosik"
+__copyright__ = "Copyright (c) 2016, UChicago Argonne, LLC."
+__docformat__ = 'restructuredtext en'
+__all__ = ['read_config',
+           'prepare_data',
+           'do_reconstruction',
+           'reconstruction']
+
 def read_config(config):
+    """
+    This function gets configuration file. It checks if the file exists and parses it into a map.
+    
+    Parameters
+    ----------
+    config : str
+        configuration file name, including path
+        
+    Returns
+    -------
+    config_map : dict
+        a map containing parsed configuration, None if the given file does not exist
+    """
+    
     if os.path.isfile(config):
         with open(config, 'r') as f:
             config_map = cfg.Config(f.read())
@@ -13,6 +92,32 @@ def read_config(config):
         return None
 
 def prepare_data(config_map, data):
+    """
+    This function prepares raw data for reconstruction. It usees configured parameters. The preparation consists of the following steps:
+    1. clearing the noise - the values below an amplitude threashold are set to zero
+    2. removing the "aliens" - aliens are areas that are effect of interferrence. The area is manually set in a configuration file 
+    after inspecting the data.
+    3. binning - adding amplitudes of several consequitive points. Binning can be done in any dimention.
+    4. amplitudes are set to sqrt
+    5. centering - finding the greatest amplitude and locating it at a center of new array. Typically several new rows/columns/slices
+    are added. These are filled with zeros. When changing the dimension the code finds the smallest possible dimension that is 
+    supported by opencl library (multiplier of 2, 3, and 5).
+    6. shift in place - shift the zero-frequency component to the center of the spectrum
+    
+    Parameters
+    ----------
+    config_map : dict
+        configuration map
+        
+    data : array
+        a 3D np array containing experiment data
+        
+    Returns
+    -------
+    data : array
+        a 3D np array containing data after the preprocessing
+    """
+    
     # zero out the noise
     data = np.where(data < config_map.amp_threshold, 0, data)
 
@@ -48,6 +153,30 @@ def prepare_data(config_map, data):
     
 
 def do_reconstruction(proc, data, conf):
+    """
+    This function calls a bridge method corresponding to the requested processor type. The bridge method is an access to the CFM
+    (Calc Fast Module). When reconstraction is completed the function retrieves results from the CFM.
+    
+    Parameters
+    ----------
+    proc : str
+        a string indicating the processor type
+        
+    data : array
+        a 3D np array containing pre-processed experiment data
+        
+    conf : dict
+        configuration map
+        
+    Returns
+    -------
+    image : array
+        a 3D np array containing reconstructed image
+        
+    er : array
+        a vector containing mean error for each iteration
+    """
+    
     if proc == 'cpu':
         from src_py.cyth.bridge_cpu import *
     elif proc == 'opencl':
@@ -70,6 +199,31 @@ def do_reconstruction(proc, data, conf):
     return image, er
     
 def reconstruction(proc, filename, conf):
+    """
+    This function is called by the user. It checks whether the data is valid and configuration file exists.
+    It calls function to pre-process the data, and then to run reconstruction.
+    The reconstruction results, image and errors are returned.
+    
+    Parameters
+    ----------
+    proc : str
+        a string indicating the processor type
+        
+    filename : str
+        name of a file containing experiment data
+        
+    conf : str
+        configuration file name
+        
+    Returns
+    -------
+    image : array
+        a 3D np array containing reconstructed image
+        
+    er : array
+        a vector containing mean error for each iteration
+    """
+
     data = ut.get_array_from_tif(filename)
     if len(data.shape) > 3:
         print "this program supports 3d images only"
