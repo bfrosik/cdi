04c7176024553ba6d27338450d1cf37534a92814
Barbara Frosik <bfrosik@anl.gov>
2016-12-02 10:15:43 -0600

    Update comments

--- include/state.hpp
+++ include/state.hpp
@@ -23,26 +23,26 @@ class Params;
 class State
 {
 private:
-    // This method is called
+    // This method updates support array dimentions.
     void UpdateSupport();
     
 public:
-    // Constructor. Takes pointer to the param object. Uses the param object to set the initial values.
+    // Constructor. Takes pointer to the Param object. Uses the Param object to set the initial values.
     State(Params *params);
     
     // Needs destructor to free allocated memory.
     ~State();
     
-    // This initializes the object. It's called by the Reconstruction object, as part of initialization.
-    // It does the following:
-    // 1. sets the number of iterations
-    // 2. creates array to store error values for each iteration
-    // 3. create initial support array based on the configured roi values
+    // This initializes the object. It does the following:
+    // - sets current algorithm
+    // - calculates and sets the number of iterations
+    // - create initial support array based on the configured roi values
     void Init(const dim4 data_dim);
     
-    // This method determines the attributes of the next state, which is next iteration.
+    // This method determines the attributes of the current state (i.e. iteration).
     // It returns false if the program reached the end of iterations, and true otherwise.
-    // It finds which algorithm will be run in this state (ER or HIO)
+    // It finds which algorithm will be run in this state .
+    // It sets the algorithm to run convolution based on state. (convolution is run at the algorithm switch)
     // It updates support at the correct iterations.
     // It finds whether the algorithm should include convolve.
     int Next();
@@ -50,7 +50,7 @@ public:
     // Returns current iteration number
     int GetCurrentIteration();
 
-    // Returns which algorithm (ER or HIO) should be run in a current state (i.e. iteration).
+    // Returns an algorithm that should be run in a current state (i.e. iteration).
     int GetCurrentAlg();
     
     // Returns true if the current state should include convolution.
@@ -62,8 +62,10 @@ public:
     // Stores the error parameter
     void RecordError(d_type error);
     
-    // Returns the support array
+    // Returns current support array
     af::array GetSupport();
+    
+    // Returns vector containing errors
     std::vector<d_type> GetErrors();
 };
 
