95f9cace40a9524414633e4924afac525ae3155e
bfrosik <bfrosik@anl.gov>
2019-01-14 14:41:56 -0600

    updated scripts after testing

---
+++ bin/cdi_window.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+#specific for xfm1
+p=$PATH
+p=${p//"anaconda2"/"anaconda3"}
+p=${p//"CXDUSER/anaconda"/"CXDUSER/CDI/anaconda"}
+export PATH=$p
+
+export LD_LIBRARY_PATH=/home/beams/CXDUSER/CDI/libconfig/lib:/usr/local/lib:/home/beams/CXDUSER/CDI/arrayfire/lib/:/local/cuda-9.0/lib64:/local/cuda-9.0/nvvm/lib64
+python bin/cdi_conf_window.py
+
+#python cdi_conf_window.py
--- bin/everything.sh
+++ bin/everything.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+#specific for xfm1
+p=$PATH
+p=${p//"anaconda2"/"anaconda3"}
+p=${p//"CXDUSER/anaconda"/"CXDUSER/CDI/anaconda"}
+export PATH=$p
 
 export LD_LIBRARY_PATH=LIB_PATH
 
@@ -8,5 +13,5 @@ prefix=$2
 scans=$3
 conf_dir=$4
 
-python everything.py $dev $prefix $scans $conf_dir
+python bin/everything.py $dev $prefix $scans $conf_dir
 
