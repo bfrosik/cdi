7c2a44887079a3d89cdafb9d5e998aaa990b374f
bfrosik <bfrosik@anl.gov>
2019-01-14 11:21:37 -0600

    cleaned up init script

--- init.sh
+++ init.sh
@@ -3,21 +3,22 @@
 
 echo -n "enter ArrayFire installation directory > "
 read af_dir
-#af_dir='/local/af'
 AF='AF_DIR'
 sed -i 's?'$AF'?'$af_dir'?g' reccdi/src_py/cyth/*.pyx
 
 echo -n "enter LibConfig installation directory > "
 read lc_dir
-#lc_dir=/local/libconfig
 LC='LC_DIR'
 sed -i 's?'$LC'?'$lc_dir'?g' reccdi/src_py/cyth/*.pyx
 
-#echo -n "enter cuda installation directory > "
-#read cuda_dir
+echo -n "enter cuda installation directory > "
+read cuda_dir
 
-export LD_LIBRARY_PATH=/local/libconfig/lib:/usr/local/lib:/local/af/lib
-#export LD_LIBRARY_PATH=$lc_dir/lib:/usr/local/lib:$af_dir/lib/:$cuda_dir/lib64:$cuda_dir/nvvm/lib64
+export LD_LIBRARY_PATH=$lc_dir/lib:/usr/local/lib:$af_dir/lib/:$cuda_dir/lib64:$cuda_dir/nvvm/lib64
+
+lib_path='LIB_PATH'
+sed -i 's?'$lib_path'?'$lc_dir/lib:/usr/local/lib:$af_dir/lib/:$cuda_dir/lib64:$cuda_dir/nvvm/lib64'?g' bin/everything.sh
+sed -i 's?'$lib_path'?'$lc_dir/lib:/usr/local/lib:$af_dir/lib/:$cuda_dir/lib64:$cuda_dir/nvvm/lib64'?g' bin/run_rec.sh
 
 echo -n "enter data type (float/double) > "
 read data_type
@@ -27,4 +28,5 @@ sed -i 's?'$def'?'$data_type'?g' reccdi/src_py/cyth/*.pyx
 sed -i 's?'$def'?'$data_type'?g' reccdi/include/common.h
 
 
-#python setup.py build_ext --inplace
+python setup.py build_ext --inplace
+python setup.py install
