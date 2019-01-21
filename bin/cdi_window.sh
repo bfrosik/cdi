ea5bf21dc5e499bc94f269d4a79eaad124b421b3
bfrosik <bfrosik@anl.gov>
2019-01-21 15:42:25 -0600

    removed gui code from beamline dir

--- bin/cdi_window.sh
+++ bin/cdi_window.sh
@@ -8,5 +8,3 @@ export PATH=$p
 
 export LD_LIBRARY_PATH=/home/beams/CXDUSER/CDI/libconfig/lib:/usr/local/lib:/home/beams/CXDUSER/CDI/arrayfire/lib/:/local/cuda-9.0/lib64:/local/cuda-9.0/nvvm/lib64
 python bin/cdi_conf_window.py
-
-#python cdi_conf_window.py
