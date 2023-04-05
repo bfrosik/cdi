899bf8048688fdf4670c1f571cd86b477afedc84
Barbara Frosik <bfrosik@mikolow.aps.anl.gov>
2023-04-05 10:32:04 -0500

    1test

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
+# 
 setup(ext_modules = cythonize(
            ["src_py/cyth/bridge_cpu.pyx", "src_py/cyth/bridge_opencl.pyx", "src_py/cyth/bridge_cuda.pyx",],
            language="c++",)
